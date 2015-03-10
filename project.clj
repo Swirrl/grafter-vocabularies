b6d5fc83421c6e43f71f4bf9bebb62943678a5f4
Rick Moynihan <rick.m@swirrl.com>
2015-03-10 17:02:43 +0000

    Add clojars to deploy-repositories

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://grafter.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :deploy-repositories [["releases" :clojars]]
+
   :dependencies [[org.clojure/clojure "1.6.0"]])
