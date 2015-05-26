97c78a17ee573465f1ee813085162f75189245ee
Robin Gower <robin.git@infonomics.ltd.uk>
2015-05-26 15:52:38 +0100

    Add snapshots repo

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :deploy-repositories [["releases" :clojars]]
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
 
   :dependencies [[org.clojure/clojure "1.6.0"]])
