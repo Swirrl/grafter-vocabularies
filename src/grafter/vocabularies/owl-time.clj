4764a9dca72f24843c5910783aed2e71f0e3ebb6
Robin Gower <robin.git@infonomics.ltd.uk>
2016-02-02 17:53:05 +0100

    Move time to owl-time to avoid conflicts with clojure.core/time

---
+++ src/grafter/vocabularies/owl-time.clj
@@ -0,0 +1,10 @@
+(ns grafter.vocabularies.owl-time
+  "Some convenience terms for the time ontology."
+  (:require [grafter.vocabularies.core :refer :all]))
+
+(def owl-time (prefixer "http://www.w3.org/2006/owl-time#"))
+(def owl-time:hasBeginning (owl-time "hasBeginning"))
+(def owl-time:hasEnd (owl-time "hasEnd"))
+(def owl-time:inXSDDateTime (owl-time "inXSDDateTime"))
+(def owl-time:Interval (owl-time "Interval"))
+(def owl-time:Instant (owl-time "Instant"))
