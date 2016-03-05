2d2392d07112f448324597f1c5d20059b92557c4
Rick Moynihan <rick.m@swirrl.com>
2016-03-05 00:05:40 +0000

    Add xsd uris for basic types

--- src/grafter/vocabularies/xsd.clj
+++ src/grafter/vocabularies/xsd.clj
@@ -3,3 +3,27 @@
   (:require [grafter.vocabularies.core :refer :all]))
 
 (def xsd (prefixer "http://www.w3.org/2001/XMLSchema#"))
+
+(def xml11-2 (prefixer "http://www.w3.org/TR/xmlschema11-2/#"))
+
+(def xsd:boolean (xsd "boolean"))
+
+(def xsd:byte (xsd "byte"))
+
+(def xsd:short (xsd "short"))
+
+(def xsd:decimal (xsd "decimal"))
+
+(def xsd:double (xsd "double"))
+
+(def xsd:float (xsd "float"))
+
+(def xsd:integer (xsd "integer"))
+
+(def xsd:int (xsd "int"))
+
+(def xml11-2:string (xsd "string"))
+
+(def xsd:string (xsd "string"))
+
+(def xsd:dateTime (xsd "dateTime"))
