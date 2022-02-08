082ec0a05651316cd406faa13cc23671bfd83827
Rick Moynihan <rick.m@swirrl.com>
2022-02-08 17:45:03 +0000

    Version 0.3.10-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grafter/vocabularies "0.3.9"
+(defproject grafter/vocabularies "0.3.10-SNAPSHOT"
   :description "Vocabularies for grafting Linked-data"
   :url "http://grafter.org"
   :license {:name "Eclipse Public License"
